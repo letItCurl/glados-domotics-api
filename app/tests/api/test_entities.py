ea8894b329988ac5c490a97c95167995e3d23d4c
letItCurl <rolandlopez.developer@gmail.com>
2022-10-27 09:09:32 +0700

    ✅ [STEP 2] Add test for get_entities with status filter.

--- app/tests/api/test_entities.py
+++ app/tests/api/test_entities.py
@@ -96,3 +96,26 @@ def test_get_entities_with_type_filter(client, entities, mocker):
             "created_at": mocker.ANY
         }
     ]
+
+def test_get_entities_with_status_filter(client, entities, mocker):
+    response = client.get("/entities?status=on")
+
+    assert response.status_code == 200
+    assert response.json == [
+        {
+            "id": "00000000-0000-0000-0000-000000000002",
+            "name": "Lamp",
+            "type": "light",
+            "status": "on",
+            "value": "200",
+            "created_at": mocker.ANY
+        },
+        {
+            "id": "00000000-0000-0000-0000-000000000003",
+            "name": "Thermometer",
+            "type": "sensor",
+            "status": "on",
+            "value": "28",
+            "created_at": mocker.ANY
+        }
+    ]
